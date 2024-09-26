6e2660263fc2ea9e826e102eccb283888ed6384b
rincewind-spec <bjacobs001@icloud.com>
2024-09-26 19:02:34 -0400

    Hopefully debugged slope floors

--- tools_graph.py
+++ tools_graph.py
@@ -778,18 +778,25 @@ def are_points_3d(point_list) :
 def make_sloped_geometry(point_list, thickness) :
     original_length = len(point_list)
     faces = []
-    faces[0] = list(range(len(point_list)))
-    faces[1] = list(range(original_length, original_length * 2))
+    edges = []
+    faces.append(list(range(len(point_list))))
+    faces.append(list(range(original_length, original_length * 2)))
     original_max_index = original_length - 1
     for i in range(original_length, original_length * 2) :
         point = point_list[i - original_length]
-        point_list[i] = [point[0], point[1], point[2] - thickness]
+        point_list.append([point[0], point[1], point[2] - thickness])
     for j in range(0, original_max_index) :
-        faces[j + 2] = [j, j + 1, j + 1 + original_length, j + original_length]
-    faces[len(faces)] = [original_max_index, 0, original_length, original_length + original_max_index]
-    context_source = ifcopenshell.api.context.add_context(IFC_MODEL, context_type="Model")
-    context = ifcopenshell.api.context.add_context(IFC_MODEL, context_type="Model", context_identifier="Body", target_view="MODEL_VIEW", parent=context_source)
-    return ifcopenshell.api.geometry.add_mesh_representation(IFC_MODEL, context=context, vertices=point_list, faces=faces)
+        faces.append([j, j + 1, j + 1 + original_length, j + original_length])
+        edges.append([j, j + 1])
+        edges.append([j, j + original_length])
+        edges.append(j + original_length, j + original_length + 1)
+    faces.append([original_max_index, 0, original_length, original_length + original_max_index])
+    edges.append([original_max_index, 0])
+    edges.append([original_max_index, original_max_index + original_length])
+    edges.append([original_max_index + original_length, original_length])
+    context_source = ifcopenshell.api.context.add_context(IFC_MODEL.ifcfile, context_type="Model")
+    context = ifcopenshell.api.context.add_context(IFC_MODEL.ifcfile, context_type="Model", context_identifier="Body", target_view="MODEL_VIEW", parent=context_source)
+    return ifcopenshell.api.geometry.add_mesh_representation(IFC_MODEL.ifcfile, context=context, vertices=point_list, faces=faces, edges=edges)
 @tool
 def create_roof(story_n: int = 1, point_list: list = [(0, 0, 0), (0, 100, 0), (100, 100, 0), (100, 0, 0)], roof_thickness: float = 1.0, material: str = None, offset: float = 0.0) -> bool:
     """
