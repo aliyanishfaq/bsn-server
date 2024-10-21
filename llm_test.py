58eb326df1098570160d7317e972db3afc97c7df
rincewind-spec <bjacobs001@icloud.com>
2024-10-21 12:19:00 -0400

    Testing plans created

---
+++ llm_test.py
@@ -0,0 +1,5 @@
+"""
+Plan for this file: write test cases that cover LLM interactions with the model via LangSmith: 
+https://www.langchain.com/langsmith (from the same vendor as lang_chain).
+Note: This file may not use unittest, if it is deemed necessary.
+"""
---
+++ model_test.py
@@ -0,0 +1,7 @@
+"""
+Plan for this file: write test cases covering the combined functions of tools_graph.py and ifc.py 
+hereafter referred to as the model.
+TODO:
+Refactor tools_graph.py to split out versions of the tools that a. accept an IFC_MODEL as a parameter and b.
+return the GlobalId of whatever object is created as prerequisites for testing.
+"""
