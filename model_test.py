d8348540882487eeaeabf5df119ee882cb4df7d3
rincewind-spec <bjacobs001@icloud.com>
2024-11-04 17:59:43 -0500

    beam testing done (except for profiles)

--- model_test.py
+++ model_test.py
@@ -8,7 +8,8 @@ return the GlobalId of whatever object is created as prerequisites for testing. 
 from ifc import IfcModel
 from tools_graph import session_create, wall_create, floor_create, roof_create, strip_footing_create, isolated_footing_create, beam_create, column_create, grid_create, create_story
 import unittest
-import ifcopenshell
+import ifcopenshell.util.element
+import ifcopenshell.util.placement
 
 class ModelTest(unittest.TestCase) :
     def setUp(self) :
@@ -19,5 +20,14 @@ class ModelTest(unittest.TestCase) :
         self.assertIsNotNone(story, "Story access failed")
         self.assertEqual("Level 1", story.Name, "Story naming failed")
         self.assertEqual(0.25, story.Elevation, "Story elevation failed")
+    def test_beam_creation(self) :
+        self.assertTrue(beam_create("test", self.model, "5,0,0", "8,4,0", "W16X40", "steel"), "Beam creation failed")
+        beam = self.ifcfile.by_type("IfcBeam")[0]
+        self.assertIsNotNone(beam, "Beam access failed")
+        self.assertEqual(ifcopenshell.util.element.get_elements_by_material(self.model.ifcfile, self.model.materials["steel"][0])[0], beam, "Material assignment failed")
+        placement = ifcopenshell.util.placement.get_local_placement(beam.ObjectPlacement)
+        self.assertEqual((5.0, 0.0, 0.0), (placement[0][3], placement[1][3], placement[2][3]), "Beam placement not exacty")
+        self.assertEqual(5.0, beam.Representation.Representations[0].Depth, "Beam length failed")
+        # Todo: figure out how to test profiles
 if __name__ == '__main__' :
     unittest.main()
